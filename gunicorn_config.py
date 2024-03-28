4f0ff69e51f32150d13b6cd314ec68427ec5d426
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-28 21:08:48 +0000

    changing monkey patch

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,7 @@
 # gunicorn_config.py
+import gevent.monkey
+gevent.monkey.patch_all()
+
 import multiprocessing
 
 # Basic bind configuration - IP and port your app will listen on
@@ -9,13 +12,12 @@ workers = multiprocessing.cpu_count() * 2 + 1  # Recommended formula for the num
 worker_class = 'gevent'  # Using gevent for async capabilities, adjust if you're not using async
 
 # Logging configurations
-loglevel = 'info' # to see the info 'info'
-accesslog = '-' # to see the logs '-'
+loglevel = 'info'  # to see the info 'info'
+accesslog = '-'  # to see the logs '-'
 errorlog = '-'
 
 # Secure scheme headers and proxy settings, useful if you're behind a reverse proxy
 forwarded_allow_ips = '*'  # Trust the `X-Forwarded-For` header from all IPs
 secure_scheme_headers = {
     'X-Forwarded-Proto': 'https'
-} 
-# to start gunicorn -c gunicorn_config.py app:app
+}
