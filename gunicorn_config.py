09def6d6b3be42e4b4d1eec2d6e3ff5ebb217c90
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-28 14:19:15 +0000

    changes done

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -9,12 +9,13 @@ workers = multiprocessing.cpu_count() * 2 + 1  # Recommended formula for the num
 worker_class = 'gevent'  # Using gevent for async capabilities, adjust if you're not using async
 
 # Logging configurations
-loglevel = 'warning'
-accesslog = 'None'
+loglevel = 'info' # to see the info 'info'
+accesslog = '-' # to see the logs '-'
 errorlog = '-'
 
 # Secure scheme headers and proxy settings, useful if you're behind a reverse proxy
 forwarded_allow_ips = '*'  # Trust the `X-Forwarded-For` header from all IPs
 secure_scheme_headers = {
     'X-Forwarded-Proto': 'https'
-}
+} 
+# to start gunicorn -c gunicorn_config.py app:app
